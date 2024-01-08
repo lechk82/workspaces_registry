29180eb0aab940919ef4d6ba7e3fde52210ecc7c
lechk82 <lechk82@googlemail.com>
2024-01-08 23:37:32 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Lechs Kasm worspaces',
+    description: 'Lechs Kasm worspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://lechk82.github.io/workspaces_registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/workspaces_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
